ccf44fa5b28c52014cd4dca5c8df492ae2e41082
Tammy Ho <momo09041027@gmail.com>
2025-05-07 18:48:06 +0800

    style: 新增搜尋欄樣式

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,21 @@
 $(document).ready(function () {
 
+    // 露營地JSON
     $.ajax({
-        url: './assets/data/campgrounds.json', // 確保檔案路徑正確
+        url: './assets/data/campgrounds.json',
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            console.log(data[0]);
+        },
+        error: function (err) {
+            console.error('Error fetching data:', err);
+        }
+    });
+
+    // 步道JSON
+    $.ajax({
+        url: './assets/data/hikingRoad.json', 
         type: 'GET',
         dataType: 'json',
         success: function (data) {
